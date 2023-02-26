c0e91d4d1be51b884795586b0104ccdca831e963
besthorn <zbesthorn@gmail.com>
2023-02-26 21:05:58 +0400

    10 урок

--- flsite.py
+++ flsite.py
@@ -1,6 +1,8 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, flash, abort, redirect, url_for, make_response
+from FDataBase import FDataBase
+
 
 DATABASE = '/tmp/flsite.db'
 DEBUG = True
@@ -26,22 +28,57 @@ def create_db():
     db.commit()
     db.close()
 
+
 def get_db():
     '''Соединение с БД, если оно еще не установлено'''
     if not hasattr(g, 'link_db'):
         g.link_db = connect_db()
     return g.link_db
 
+
 @app.route("/")
 def index():
     db = get_db()
-    return render_template('index.html', menu=[])
+    dbase = FDataBase(db)
+    return render_template('index.html', menu = dbase.getMenu(), posts=dbase.getPostsAnonce())
+
 
 @app.teardown_appcontext
 def close_db(error):
     '''Закрываем соединение с БД, если оно было установлено'''
     if hasattr(g, 'link_db'):
         g.link_db.close()
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+
+    return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
+
+
+@app.route("/post/<int:id_post>")
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+
 
 
 # menu = [{"name": "Установка", "url": "install-flask"},
