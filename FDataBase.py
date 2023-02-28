0943692ecb6eec1a895860eb1f56e2f47564d3b3
besthorn <zbesthorn@gmail.com>
2023-02-28 21:16:13 +0400

    10.5 урок

--- FDataBase.py
+++ FDataBase.py
@@ -5,7 +5,6 @@ import re
 from flask import url_for
 
 
-
 class FDataBase:
     def __init__(self, db):
         self.__db = db
@@ -21,10 +20,16 @@ class FDataBase:
             print("Ошибка чтения из БД")
         return []
 
-    def addPost(self, title, text):
+    def addPost(self, title, text, url):
         try:
+            self.__cur.execute("SELECT COUNT() as `count` FROM posts WHERE url LIKE '{url}'")
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("Статья с таким url уже существует")
+                return False
+
             tm = math.floor(time.time())
-            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?)", (title, text, tm))
+            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)", (title, text, url, tm))
             self.__db.commit()
         except sqlite3.Error as e:
             print("Ошибка добавления статьи в БД " + str(e))
@@ -32,9 +37,9 @@ class FDataBase:
 
         return True
 
-    def getPost(self, postId):
+    def getPost(self, alias):
         try:
-            self.__cur.execute(f"SELECT title, text FROM posts WHERE id = {postId} LIMIT 1")
+            self.__cur.execute(f"SELECT title, text FROM posts WHERE url LIKE  '{alias}' LIMIT 1")
             res = self.__cur.fetchone()
             if res:
                 return res
@@ -45,10 +50,11 @@ class FDataBase:
 
     def getPostsAnonce(self):
         try:
-            self.__cur.execute(f"SELECT id, title, text FROM posts ORDER BY time DESC")
+            self.__cur.execute(f"SELECT id, title, text, url FROM posts ORDER BY time DESC")
             res = self.__cur.fetchall()
-            if res: return res
+            if res:
+                return res
         except sqlite3.Error as e:
             print("Ошибка получения статьи из БД " + str(e))
 
-        return []
+        return (False, False)
--- flsite.py
+++ flsite.py
@@ -57,7 +57,7 @@ def addPost():
 
     if request.method == "POST":
         if len(request.form['name']) > 4 and len(request.form['post']) > 10:
-            res = dbase.addPost(request.form['name'], request.form['post'])
+            res = dbase.addPost(request.form['name'], request.form['post'], request.form['url'])
             if not res:
                 flash('Ошибка добавления статьи', category='error')
             else:
@@ -68,66 +68,16 @@ def addPost():
     return render_template('add_post.html', menu=dbase.getMenu(), title="Добавление статьи")
 
 
-@app.route("/post/<int:id_post>")
-def showPost(id_post):
+@app.route("/post/<alias>")
+def showPost(alias):
     db = get_db()
     dbase = FDataBase(db)
-    title, post = dbase.getPost(id_post)
+    title, post = dbase.getPost(alias)
     if not title:
         abort(404)
 
     return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 
-
-
-# menu = [{"name": "Установка", "url": "install-flask"},
-#         {"name": "Первое приложение", "url": "first-app"},
-#         {"name": "Обратная связь", "url": "contact"}]
-#
-#
-# @app.route("/")
-# def index():
-#     return render_template('index.html', menu=menu)
-#
-#
-# @app.route("/about")
-# def about():
-#     return render_template('about.html', title="О сайте", menu=menu)
-#
-#
-# @app.route("/contact", methods=["POST", "GET"])
-# def contact():
-#     if request.method == 'POST':
-#         if len(request.form['username']) > 2:
-#             flash('Сообщение отправлено', category='success')
-#         else:
-#             flash('Ошибка отправки', category='error')
-#
-#     return render_template('contact.html', title="Обраиная связь", menu=menu)
-#
-#
-# @app.route("/profile/<username>")
-# def profile(username):
-#     if 'userLogged' not in session or session['userLogged'] != username:
-#         abort(401)
-#
-#     return f"Пользователь: {username}"
-#
-# @app.route("/login", methods=["POST", "GET"])
-# def login():
-#     if 'userLogged' in session:
-#         return redirect(url_for('profile', username=session['userLogged']))
-#     elif request.method == 'POST' and request.form['username'] == "horn" and request.form['psw'] == "123":
-#         session['userLogged'] = request.form['username']
-#         return redirect(url_for('profile', username=session['userLogged']))
-#
-#     return render_template('login.html', title="Авторизация", menu=menu)
-#
-# @app.errorhandler(404)
-# def pageNotFount(error):
-#     return render_template('page404.html', title="Страница не найдена", menu=menu), 404
-#
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
